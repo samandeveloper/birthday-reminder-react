51ec82da9de2364c288091d52218a68b179c7ac6
Saman M. Mohaghegh <sn.mohaghegh@gmail.com>
2022-12-07 15:51:44 -0700

    Updates

---
+++ precache-manifest.aef437844ceb35f9c11d461c5c03fae5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff5b63c83e6da0f78b3119b24a8a2a96",
+    "url": "/birthday-reminder-react/index.html"
+  },
+  {
+    "revision": "d04fac9795a85bd54e4a",
+    "url": "/birthday-reminder-react/static/css/main.a25574c1.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/birthday-reminder-react/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/birthday-reminder-react/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d04fac9795a85bd54e4a",
+    "url": "/birthday-reminder-react/static/js/main.ee5605b0.chunk.js"
+  },
+  {
+    "revision": "53fd85258c192f755c3b",
+    "url": "/birthday-reminder-react/static/js/runtime-main.9a26030c.js"
+  }
+]);
